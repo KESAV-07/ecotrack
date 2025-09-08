5cda8a7f498467df17140708e63544bfce74a59b
aravindh-bloop <152676926+aravindh-bloop@users.noreply.github.com>
2025-09-08 13:03:01 +0530

    linked all webpages

--- app.py
+++ app.py
@@ -3,12 +3,31 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
+@app.route("/home")
 def home():
     return render_template("index.html")
 
-@app.route("/second")
+@app.route("/lessons")
 def second():
-    return render_template("2nd.html")
+    return render_template("lessons.html")
+
+@app.route("/quizzes")
+def third():
+    return render_template("quizzes.html")
+
+@app.route("/ecotasks")
+def fourth():
+    return render_template("ecotasks.html")
+
+@app.route("/progress")
+def fifth():
+    return render_template("progress.html")
+
+@app.route("/leaderboards")
+def sixth():
+    return render_template("leaderboards.html")
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
