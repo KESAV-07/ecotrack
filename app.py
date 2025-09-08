89368336a72560ee8f1b3237d3f95d4b5767ffc6
aravindh-bloop <152676926+aravindh-bloop@users.noreply.github.com>
2025-09-08 10:02:06 +0530

    new homepg

--- app.py
+++ app.py
@@ -1,1 +1,15 @@
-#hi
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/second")
+def second():
+    return render_template("2nd.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
